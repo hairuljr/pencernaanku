b3924b69d1e03e212ae9e022e5955b18106fd13b
hairuljr <januarhairul@gmail.com>
2019-11-01 12:53:55 +0700

    Membuat controller Admin serta method indexnya
    Membuat Controller Admin sekaligus method index
    Bongkar template Gentelella untuk memisahkan setiap bagian yang bisa dipakai berulang-ulang.
    header, topbar, sidebar dan footer ke dalam folder views/templates

---
+++ application/views/admin/index.php
@@ -0,0 +1,39 @@
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="row top_tiles">
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-user-md"></i></div>
+                  <div class="count">12</div>
+                  <h3>Diagnosa</h3>
+                  <!-- <p>Lorem ipsum psdea itgum rixt.</p> -->
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-users"></i></div>
+                  <div class="count">10</div>
+                  <h3>User</h3>
+                  <!-- <p>Lorem ipsum psdea itgum rixt.</p> -->
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-newspaper-o"></i></div>
+                  <div class="count">5</div>
+                  <h3>Artikel</h3>
+                  <!-- <p>Lorem ipsum psdea itgum rixt.</p> -->
+                </div>
+              </div>
+              <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="tile-stats">
+                  <div class="icon"><i class="fa fa-medkit"></i></div>
+                  <div class="count">15</div>
+                  <h3>Penyakit</h3>
+                  <!-- <p>Lorem ipsum psdea itgum rixt.</p> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,54 @@
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Copyright &copy Admin SP <?= date('Y'); ?>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+        </div>
+        </div>
+
+        <!-- jQuery -->
+        <script src="<?= base_url('assets'); ?>/vendors/jquery/dist/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="<?= base_url('assets'); ?>/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+        <!-- FastClick -->
+        <script src="<?= base_url('assets'); ?>/vendors/fastclick/lib/fastclick.js"></script>
+        <!-- NProgress -->
+        <script src="<?= base_url('assets'); ?>/vendors/nprogress/nprogress.js"></script>
+        <!-- Chart.js -->
+        <script src="<?= base_url('assets'); ?>/vendors/Chart.js/dist/Chart.min.js"></script>
+        <!-- jQuery Sparklines -->
+        <script src="<?= base_url('assets'); ?>/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+        <!-- morris.js -->
+        <script src="<?= base_url('assets'); ?>/vendors/raphael/raphael.min.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/morris.js/morris.min.js"></script>
+        <!-- gauge.js -->
+        <script src="<?= base_url('assets'); ?>/vendors/gauge.js/dist/gauge.min.js"></script>
+        <!-- bootstrap-progressbar -->
+        <script src="<?= base_url('assets'); ?>/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+        <!-- Skycons -->
+        <script src="<?= base_url('assets'); ?>/vendors/skycons/skycons.js"></script>
+        <!-- Flot -->
+        <script src="<?= base_url('assets'); ?>/vendors/Flot/jquery.flot.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/Flot/jquery.flot.pie.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/Flot/jquery.flot.time.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/Flot/jquery.flot.stack.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/Flot/jquery.flot.resize.js"></script>
+        <!-- Flot plugins -->
+        <script src="<?= base_url('assets'); ?>/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/flot.curvedlines/curvedLines.js"></script>
+        <!-- DateJS -->
+        <script src="<?= base_url('assets'); ?>/vendors/DateJS/build/date.js"></script>
+        <!-- bootstrap-daterangepicker -->
+        <script src="<?= base_url('assets'); ?>/vendors/moment/min/moment.min.js"></script>
+        <script src="<?= base_url('assets'); ?>/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+        <!-- Custom Theme Scripts -->
+        <script src="<?= base_url('assets'); ?>/build/js/custom.min.js"></script>
+
+        </body>
+
+        </html>

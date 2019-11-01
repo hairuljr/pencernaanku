0381f66ab671dc516c1b79794ddfe163cd5cccb4
hairuljr <januarhairul@gmail.com>
2019-11-01 11:03:48 +0700

    Membuat fungsi index untuk login
    Membuat fungsi/method index untuk login
    Menambahkan view untuk form login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,7 +8,25 @@ class Auth extends CI_Controller
 		parent::__construct();
 		$this->load->library('form_validation');
 		$this->load->model('Auth_model', 'auth');
+  }
+  
+  public function index()
+	{
+		//form validasi
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+		//jika validasi gagal kembalikn ke halaman login
+		if ($this->form_validation->run() == false) {
+			$data['judul'] = 'Login Page';
+			$this->load->view('auth/login', $data);
+
+			//jika validasi sukses menuju ke method _login
+		} else {
+			$this->_login();
+		}
 	}
+
   public function registrasi()
 	{
 		//form validasi
---
+++ application/views/auth/login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title><?= $judul; ?></title>
+
+  <!-- Bootstrap -->
+  <link href="<?= base_url('assets'); ?>/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link href="<?= base_url('assets'); ?>/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <!-- NProgress -->
+  <link href="<?= base_url('assets'); ?>/vendors/nprogress/nprogress.css" rel="stylesheet">
+  <!-- Animate.css -->
+  <link href="<?= base_url('assets'); ?>/vendors/animate.css/animate.min.css" rel="stylesheet">
+
+  <!-- Custom Theme Style -->
+  <link href="<?= base_url('assets'); ?>/build/css/custom.min.css" rel="stylesheet">
+</head>
+
+<body class="login">
+  <div>
+    <div class="login_wrapper">
+      <div class="animate form login_form">
+        <section class="login_content">
+          <form method="POST" action="<?= base_url('auth'); ?>">
+            <h1>Login Form</h1>
+            <?= $this->session->flashdata('message'); ?>
+            <div>
+              <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+              <input type="text" name="email" class="form-control" placeholder="Email" value="<?= set_value('email'); ?>">
+            </div>
+            <div>
+              <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+              <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+            <div>
+              <button class="btn btn-default submit" type="submit">Log in</button>
+              <!-- <a class="reset_pass" href="#">Lost your password?</a> -->
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="separator">
+              <p class="change_link">New to site?
+                <a href="<?= base_url('auth/registrasi'); ?>" class="to_register"> Create Account </a>
+              </p>
+
+              <div class="clearfix"></div>
+              <br />
+            </div>
+          </form>
+        </section>
+      </div>
+
+    </div>
+  </div>
+</body>
+
+</html>

83163e6dbec24204b6a8eb23c6727e31e978ceb4
hairuljr <januarhairul@gmail.com>
2019-11-02 12:14:36 +0700

    Update data_gejala.php

--- application/views/admin/data_gejala.php
+++ application/views/admin/data_gejala.php
@@ -23,62 +23,41 @@
                     <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                       <thead>
                         <tr>
-                          <th>First name</th>
-                          <th>Last name</th>
-                          <th>Position</th>
-                          <th>Office</th>
-                          <th>Age</th>
-                          <th>Start date</th>
-                          <th>Salary</th>
-                          <th>Extn.</th>
-                          <th>E-mail</th>
+                          <th>No</th>
+                          <th>Kode</th>
+                          <th>Gejala</th>
+                          <th>Aksi</th>
                         </tr>
                       </thead>
                       <tbody>
+                        <?php $i = 1; ?>
                         <tr>
-                          <td>Tiger</td>
-                          <td>Nixon</td>
-                          <td>System Architect</td>
-                          <td>Edinburgh</td>
-                          <td>61</td>
-                          <td>2011/04/25</td>
-                          <td>$320,800</td>
-                          <td>5421</td>
-                          <td>t.nixon@datatables.net</td>
+                          <td><?= $i; ?></td>
+                          <td>G01</td>
+                          <td>Sakit Perut</td>
+                          <td>
+                            <a href="" class="badge badge-success" data-toggle="modal" data-target="#editUserModal">edit</a>
+                            <a href="" class="badge badge-danger" onclick="return confirm('Anda yakin ingin hapus user ini?');">delete</a>
+                          </td>
                         </tr>
                         <tr>
-                          <td>Garrett</td>
-                          <td>Winters</td>
-                          <td>Accountant</td>
-                          <td>Tokyo</td>
-                          <td>63</td>
-                          <td>2011/07/25</td>
-                          <td>$170,750</td>
-                          <td>8422</td>
-                          <td>g.winters@datatables.net</td>
+                          <td><?= $i; ?></td>
+                          <td>G02</td>
+                          <td>Sakit Hati</td>
+                          <td>
+                            <a href="" class="badge badge-success" data-toggle="modal" data-target="#editUserModal">edit</a>
+                            <a href="" class="badge badge-danger" onclick="return confirm('Anda yakin ingin hapus user ini?');">delete</a></td>
                         </tr>
                         <tr>
-                          <td>Ashton</td>
-                          <td>Cox</td>
-                          <td>Junior Technical Author</td>
-                          <td>San Francisco</td>
-                          <td>66</td>
-                          <td>2009/01/12</td>
-                          <td>$86,000</td>
-                          <td>1562</td>
-                          <td>a.cox@datatables.net</td>
+                          <td><?= $i; ?></td>
+                          <td>G03</td>
+                          <td>Sakit Jiwa</td>
+                          <td>
+                            <a href="" class="badge badge-success" data-toggle="modal" data-target="#editUserModal">edit</a>
+                            <a href="" class="badge badge-danger" onclick="return confirm('Anda yakin ingin hapus user ini?');">delete</a>
+                          </td>
                         </tr>
-                        <tr>
-                          <td>Cedric</td>
-                          <td>Kelly</td>
-                          <td>Senior Javascript Developer</td>
-                          <td>Edinburgh</td>
-                          <td>22</td>
-                          <td>2012/03/29</td>
-                          <td>$433,060</td>
-                          <td>6224</td>
-                          <td>c.kelly@datatables.net</td>
-                        </tr>
+                        <?php $i++; ?>
                       </tbody>
                     </table>
                   </div>
